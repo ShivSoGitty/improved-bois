d48c9dae40dd0c8759fcb62b961072df982565fb
ShivSoGitty <86866438+ShivSoGitty@users.noreply.github.com>
2022-01-20 15:49:13 +1100

    Add files via upload

---
+++ Let's Chat.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDNuTlgvYuFqaxOGsyTcjSUyHptLQl1nWo",
+    authDomain: "poggers-a64b1.firebaseapp.com",
+    projectId: "poggers-a64b1",
+    storageBucket: "poggers-a64b1.appspot.com",
+    messagingSenderId: "393542996825",
+    appId: "1:393542996825:web:6ed2be93ef9e98dcfcf248"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  
